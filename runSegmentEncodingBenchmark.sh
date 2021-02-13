2622d5cf569762eeef9d85fbdfeec3925cb627c3
Ben Hurdelhey <26392770+ben243871694@users.noreply.github.com"$REMOTE" -ancestor "$BASE" -merge "$MERGED>
2021-02-13 21:33:27 +0000

    dirty hack so the cmakelists is not overwritten

--- runSegmentEncodingBenchmark.sh
+++ runSegmentEncodingBenchmark.sh
@@ -15,9 +15,19 @@ run_benchmark() {
 rm -rf hyriseColumnCompressionBenchmark
 git clone git@github.com:benrobby/hyrise.git hyriseColumnCompressionBenchmark
 cd hyriseColumnCompressionBenchmark
-
+    # Checkout Git
+    git checkout  benchmark/compareColumnEncodings && git pull && git submodule init && git submodule update
 git checkout benchmark/compareColumnEncodings && cp FastPFOR_CMakeLists.txt third_party/FastPFor/CMakeLists.txt
-run_benchmark benchmark/compareColumnEncodings fastpfor
+    # Run Benchmark without LTO
+    mkdir -p cmake-build-release && cd cmake-build-release
+    rm -rf *
+    cmake .. -DCMAKE_C_COMPILER=clang-10 -DCMAKE_CXX_COMPILER=clang++-10 -DCMAKE_BUILD_TYPE=Release -DHYRISE_RELAXED_BUILD=On -GNinja && ninja hyriseBenchmarkTPCH &&
+    ./hyriseBenchmarkTPCH -e ../encoding_fastpfor.json --dont_cache_binary_tables -o ../tpch_fastpfor_1.json -r 100  >> ../sizes_fastpfor.txt
+    # Run Benchmark with LTO 
+    cmake ..  -DCMAKE_C_COMPILER=clang-10 -DCMAKE_CXX_COMPILER=clang++-10 -DENABLE_LTO=On -DCMAKE_BUILD_TYPE=Release -DHYRISE_RELAXED_BUILD=On -GNinja && ninja hyriseBenchmarkTPCH &&
+    ./hyriseBenchmarkTPCH -e ../encoding_fastpfor.json --dont_cache_binary_tables -o ../tpch_fastpfor_LTO_1.json -r 100
+    cd ..
+
 run_benchmark benchmark/implementSIMDCAI SIMDCAI
 
 git checkout benchmark/turboPFOR && git pull &&
