f197824cd0e3e0cc22108ad374c7bb95a6e350d7
Ben Hurdelhey <26392770+ben243871694@users.noreply.github.com"$REMOTE" -ancestor "$BASE" -merge "$MERGED>
2021-02-15 15:36:56 +0000

    build simdcai

--- runSegmentEncodingBenchmarkMultithreaded.sh
+++ runSegmentEncodingBenchmarkMultithreaded.sh
@@ -2,16 +2,15 @@ run_benchmark() {
     # Checkout Git
     git checkout $1 && git pull && git submodule init && git submodule update
     # Run Benchmark without LTO
-    if [ -n "$3" ]; then    
-         eval $3 && echo "setup"
+    if [ -n "$3" ]; then
+       eval $3
     fi
     mkdir -p cmake-build-release && cd cmake-build-release
     rm -rf *
     cmake .. -DCMAKE_C_COMPILER=clang-10 -DCMAKE_CXX_COMPILER=clang++-10 -DCMAKE_BUILD_TYPE=Release -DHYRISE_RELAXED_BUILD=On -GNinja && ninja hyriseBenchmarkTPCH &&
-    ./hyriseBenchmarkTPCH -e ../encoding_$2.json --dont_cache_binary_tables -o ../tpch_$2_1.json -r 100  >> ../sizes_$2.txt
-    # Run Benchmark with LTO 
-    cmake ..  -DCMAKE_C_COMPILER=clang-10 -DCMAKE_CXX_COMPILER=clang++-10 -DENABLE_LTO=On -DCMAKE_BUILD_TYPE=Release -DHYRISE_RELAXED_BUILD=On -GNinja && ninja hyriseBenchmarkTPCH &&
-    ./hyriseBenchmarkTPCH -e ../encoding_$2.json --dont_cache_binary_tables -o ../tpch_$2_LTO_1.json -r 100
+    ./hyriseBenchmarkTPCH -e ../encoding_$2.json --dont_cache_binary_tables -o ../tpch_$2_5.json -r 100 --scheduler --clients 5
+    ./hyriseBenchmarkTPCH -e ../encoding_$2.json --dont_cache_binary_tables -o ../tpch_$2_10.json -r 100 --scheduler --clients 10 
+    ./hyriseBenchmarkTPCH -e ../encoding_$2.json --dont_cache_binary_tables -o ../tpch_$2_20.json -r 100 --scheduler --clients 20
     cd ..
 }
 
