8d2d84740349e8e46a9a11f244b1463d7714f596
Ben Hurdelhey <26392770+ben243871694@users.noreply.github.com"$REMOTE" -ancestor "$BASE" -merge "$MERGED>
2021-02-23 00:38:06 +0000

    adds script to run greenfield benchmarks

---
+++ runGreenfieldBenchmarks.sh
@@ -0,0 +1,17 @@
+rm -rf hyriseGreenfieldBenchmark
+git clone git@github.com:benrobby/hyrise.git hyriseGreenfieldBenchmark
+cd hyriseGreenfieldBenchmark
+
+git checkout ben/benchmarking && git pull
+git submodule init && git submodule update
+cd third_party/TurboPFor && make all -j 16 && cd -
+cd third_party/SIMDCompressionAndIntersection && make all -j 16 && cd -
+cd third_party/sdsl-lite && ./install.sh ./ && cd -
+
+mkdir cmake-build-release && cd cmake-build-release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DHYRISE_RELAXED_BUILD=On -DENABLE_UNSUPPORTED_COMPILER=On -GNinja
+ninja hyriseBenchmarkColumnCompression
+
+./hyriseBenchmarkColumnCompression --benchmark_filter="" --benchmark_format=console --benchmark_out=benchmark_results.txt --benchmark_out_format=csv
+
+
