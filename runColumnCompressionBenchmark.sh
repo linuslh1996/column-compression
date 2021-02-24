3acb9bd5edca6060468496d45697dd17e9827828
Linus Heinzl <linus.heinzl@student.hpi.de>
2021-02-24 21:41:42 +0100

    Merge Multithreaded and Singlethreaded Benchmark

--- runColumnCompressionBenchmark.sh
+++ runColumnCompressionBenchmark.sh
@@ -3,27 +3,39 @@ run_benchmark() {
     git checkout $1 && git pull && git submodule init && git submodule update
     # Run Benchmark without LTO
     if [ -n "$3" ]; then
-      eval $3 && echo "setup"
+       eval $3 && echo "setup"
     fi
     mkdir -p cmake-build-release && cd cmake-build-release
     rm -rf *
-    cmake .. -DCMAKE_C_COMPILER=clang-10 -DCMAKE_CXX_COMPILER=clang++-10 -DCMAKE_BUILD_TYPE=Release -DHYRISE_RELAXED_BUILD=On -GNinja && ninja hyriseBenchmarkTPCH &&
-    ./hyriseBenchmarkTPCH -s 10 -e ../encoding_$2.json --dont_cache_binary_tables -o ../tpch_$2_1.json -r 100  >> ../sizes_$2.txt
-    # Run Benchmark with LTO 
-    cmake ..  -DCMAKE_C_COMPILER=clang-10 -DCMAKE_CXX_COMPILER=clang++-10 -DENABLE_LTO=On -DCMAKE_BUILD_TYPE=Release -DHYRISE_RELAXED_BUILD=On -GNinja && ninja hyriseBenchmarkTPCH &&
-    ./hyriseBenchmarkTPCH -s 10 -e ../encoding_$2.json --dont_cache_binary_tables -o ../tpch_$2_LTO_1.json -r 100
+    if cmake .. -DCMAKE_C_COMPILER=clang-$clang_version -DCMAKE_CXX_COMPILER=clang++-$clang_version -DCMAKE_BUILD_TYPE=Release -DHYRISE_RELAXED_BUILD=On -GNinja && ninja hyriseBenchmarkTPCH ; then
+      if [ "$run_multithreaded" = true ] ; then
+          ./hyriseBenchmarkTPCH -e ../encoding_$2.json --dont_cache_binary_tables -o ../tpch_$2_14_shuffled.json -s 10 -t 1800 --scheduler --clients 14 --mode=Shuffled
+          ./hyriseBenchmarkTPCH -e ../encoding_$2.json --dont_cache_binary_tables -o ../tpch_$2_28_shuffled.json -s 10 -t 1800 --scheduler --clients 28 --mode=Shuffled
+      else
+          ./hyriseBenchmarkTPCH -e ../encoding_$2.json --dont_cache_binary_tables -o ../tpch_$2_singlethreaded.json -t 1800
+        fi
+    fi
     cd ..
 }
 
+# Configuration
+clang_version=11
+run_multithreaded=true
+
+# Clone Hyrise Repo
 rm -rf hyriseColumnCompressionBenchmark
 git clone git@github.com:benrobby/hyrise.git hyriseColumnCompressionBenchmark
 cd hyriseColumnCompressionBenchmark
 
-run_benchmark benchmarking/bitCompression dictionary
+# Execute Benchmarks
+run_benchmark benchmarking/bitCompressionSIMDCAISeq bitpacking_simdcai_seq "cd third_party/SIMDCompressionAndIntersection && make all -j     16 && cd -"
+run_benchmark benchmarking/bitCompressionSIMDCAI bitpacking_simdcai "cd third_party/SIMDCompressionAndIntersection && make all -j     16 && cd -"
+run_benchmark benchmarking/bitCompression dictionary "cd third_party/TurboPFor-Integer-Compression && make all -j 8 && cd -"
 run_benchmark benchmarking/bitCompression bitpacking_turbopfor
 run_benchmark benchmarking/bitCompressionSequential bitpacking_turbopfor_seq
 run_benchmark benchmark/compactVetor bitpacking_compactvector
 run_benchmark benchmark/compactVectorFixed bitpacking_compactvector_f
 run_benchmark benchmark/compactVetor simdbp
-run_benchmark benchmarking/bitCompressionSIMDCAISeq bitpacking_simdcai_seq "cd third_party/SIMDCompressionAndIntersection && make all -j     16 && cd -"
-run_benchmark benchmarking/bitCompressionSIMDCAI bitpacking_simdcai "cd third_party/SIMDCompressionAndIntersection && make all -j     16 && cd -"
+
+# Process Result
+zip columncompression$(date +%Y%m%d) *.json
--- runSegmentEncodingBenchmark.sh
+++ runSegmentEncodingBenchmark.sh
@@ -2,34 +2,41 @@ run_benchmark() {
     # Checkout Git
     git checkout $1 && git pull && git submodule init && git submodule update
     # Run Benchmark without LTO
-    if [ -n "$3" ]; then    
-         eval $3 && echo "setup"
+    if [ -n "$3" ]; then
+       eval $3
     fi
     mkdir -p cmake-build-release && cd cmake-build-release
     rm -rf *
-    cmake .. -DCMAKE_C_COMPILER=clang-10 -DCMAKE_CXX_COMPILER=clang++-10 -DCMAKE_BUILD_TYPE=Release -DHYRISE_RELAXED_BUILD=On -GNinja && ninja hyriseBenchmarkTPCH &&
-    ./hyriseBenchmarkTPCH -s 10 -e ../encoding_$2.json --dont_cache_binary_tables -o ../tpch_$2_1.json -r 100  >> ../sizes_$2.txt
-    # Run Benchmark with LTO 
-    cmake ..  -DCMAKE_C_COMPILER=clang-10 -DCMAKE_CXX_COMPILER=clang++-10 -DENABLE_LTO=On -DCMAKE_BUILD_TYPE=Release -DHYRISE_RELAXED_BUILD=On -GNinja && ninja hyriseBenchmarkTPCH &&
-    ./hyriseBenchmarkTPCH -s 10 -e ../encoding_$2.json --dont_cache_binary_tables -o ../tpch_$2_LTO_1.json -r 100
+    if cmake .. -DCMAKE_C_COMPILER=clang-$clang_version -DCMAKE_CXX_COMPILER=clang++-$clang_version -DCMAKE_BUILD_TYPE=Release -DHYRISE_RELAXED_BUILD=On -GNinja && ninja hyriseBenchmarkTPCH ; then
+      if [ "$run_multithreaded" = true ] ; then
+          ./hyriseBenchmarkTPCH -e ../encoding_$2.json --dont_cache_binary_tables -o ../tpch_$2_14_shuffled.json -s 10 -t 1800 --scheduler --clients 14 --mode=Shuffled
+          ./hyriseBenchmarkTPCH -e ../encoding_$2.json --dont_cache_binary_tables -o ../tpch_$2_28_shuffled.json -s 10 -t 1800 --scheduler --clients 28 --mode=Shuffled
+      else
+          ./hyriseBenchmarkTPCH -e ../encoding_$2.json --dont_cache_binary_tables -o ../tpch_$2_singlethreaded.json -t 1800
+        fi
+    fi
     cd ..
 }
 
+# Configuration
+clang_version=11
+run_multithreaded=true
+
+# Clone Hyrise Repo
 rm -rf hyriseColumnCompressionBenchmark
 git clone git@github.com:benrobby/hyrise.git hyriseColumnCompressionBenchmark
 cd hyriseColumnCompressionBenchmark
 
-
-git checkout benchmark/turboPFOR && git pull &&
-git submodule init && git submodule update &&
-cd third_party/TurboPFor-Integer-Compression && make all -j 8 && cd - &&
-
+# Execute Benchmarks
+run_benchmark benchmark/implementSIMDCAI SIMDCAI "cd third_party/SIMDCompressionAndIntersection && make all -j 16 && cd -"
 run_benchmark benchmark/turboPFOR TurboPFOR
 run_benchmark benchmark/turboPFOR_bitpacking TurboPFOR_bitpacking
-
-run_benchmark benchmark/implementSIMDCAI SIMDCAI "cd third_party/SIMDCompressionAndIntersection && make all -j 16 && cd -"
 run_benchmark benchmark/turboPFOR Dictionary
 run_benchmark benchmark/turboPFOR FrameOfReference
 run_benchmark benchmark/turboPFOR Unencoded
 run_benchmark benchmark/turboPFOR LZ4
 run_benchmark benchmark/turboPFOR RunLength
+
+# Process Results
+zip segmentencoding$(date +%Y%m%d) *.json
+
