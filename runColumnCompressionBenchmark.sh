6ad2b0e096412fba849e5456b39427d2bd8af178
Ben Hurdelhey <26392770+ben243871694@users.noreply.github.com>
2021-02-24 22:32:48 +0100

    adds new unencoded codec and writes sizes to file

--- runColumnCompressionBenchmark.sh
+++ runColumnCompressionBenchmark.sh
@@ -12,7 +12,7 @@ run_benchmark() {
           ./hyriseBenchmarkTPCH -e ../encoding_$2.json --dont_cache_binary_tables -o ../tpch_$2_14_shuffled.json -s 10 -t 1800 --scheduler --clients 14 --mode=Shuffled
           ./hyriseBenchmarkTPCH -e ../encoding_$2.json --dont_cache_binary_tables -o ../tpch_$2_28_shuffled.json -s 10 -t 1800 --scheduler --clients 28 --mode=Shuffled
       else
-          ./hyriseBenchmarkTPCH -e ../encoding_$2.json --dont_cache_binary_tables -o ../tpch_$2_singlethreaded.json
+          ./hyriseBenchmarkTPCH -e ../encoding_$2.json --dont_cache_binary_tables -o ../tpch_$2_singlethreaded.json >> ../sizes_$2.txt
         fi
     fi
     cd ..
@@ -44,6 +44,7 @@ run_benchmark benchmarking/bitCompressionSequential bitpacking_turbopfor_seq
 run_benchmark benchmark/compactVetor bitpacking_compactvector
 run_benchmark benchmark/compactVectorFixed bitpacking_compactvector_f
 run_benchmark benchmark/compactVetor simdbp
+run_benchmark benchmarking/compressionUnencoded compressionUnencoded
 
 # Process Result
-zip -m columncompression$(date +%Y%m%d) *.json
+zip -m columncompression$(date +%Y%m%d) tpch* sizes*
