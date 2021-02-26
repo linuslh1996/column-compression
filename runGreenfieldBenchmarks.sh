3104a333417fcd44f9a2a43e491a8309958ea1f1
Ben Hurdelhey <26392770+ben243871694@users.noreply.github.com"$REMOTE" -ancestor "$BASE" -merge "$MERGED>
2021-02-26 23:33:35 +0000

    minor fixes to greenfield benchmarks

--- runGreenfieldBenchmarks.sh
+++ runGreenfieldBenchmarks.sh
@@ -13,11 +13,11 @@ cmake .. -DCMAKE_C_COMPILER=clang-10 -DCMAKE_CXX_COMPILER=clang++-10 -DCMAKE_BUI
 ninja hyriseBenchmarkColumnCompression
 ./hyriseBenchmarkColumnCompression --benchmark_filter="" --benchmark_format=console --benchmark_out=benchmark_results.txt --benchmark_out_format=csv --benchmark_min_time=0.2
 mv bits_per_int.csv bits_per_int_nolto.csv
-mv benchmark_results.txt benchmark_results_nolto.csv
+mv benchmark_results.txt benchmark_results_nolto.txt
 
 cmake .. -DCMAKE_C_COMPILER=clang-10 -DCMAKE_CXX_COMPILER=clang++-10 -DCMAKE_BUILD_TYPE=Release -DHYRISE_RELAXED_BUILD=On -DENABLE_UNSUPPORTED_COMPILER=On -DENABLE_LTO=On -GNinja --benchmark_min_time=0.2
 ninja hyriseBenchmarkColumnCompression
 ./hyriseBenchmarkColumnCompression --benchmark_filter="" --benchmark_format=console --benchmark_out=benchmark_results.txt --benchmark_out_format=csv
 
 
-zip greenfield_results.zip  bits_per_int.csv benchmark_results.txt bits_per_int_nolto.csv benchmark_results_nolto.csv
+zip greenfield_results.zip  bits_per_int.csv benchmark_results.txt bits_per_int_nolto.csv benchmark_results_nolto.txt
