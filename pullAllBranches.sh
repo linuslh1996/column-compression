9f4e44ec180accdaec63531baa1034a13fc9c036
Ben Hurdelhey <26392770+ben243871694@users.noreply.github.com"$REMOTE" -ancestor "$BASE" -merge "$MERGED>
2021-03-05 16:14:06 +0000

    adds new branches to script

--- pullAllBranches.sh
+++ pullAllBranches.sh
@@ -1,4 +1,4 @@
-for branch_name in benchmark/compactVetor benchmark/compareColumnEncodings benchmark/implementSIMDCAI benchmark/turboPFOR benchmark/turboPFOR_bitpacking benchmarking/bitCompression benchmarking/bitCompressionSIMDCAI benchmarking/bitCompressionSIMDCAISeq benchmarking/bitCompressionSequential;
+for branch_name in benchmark/compactVetor benchmark/compactVectorFixed benchmark/compareColumnEncodings benchmark/implementSIMDCAI benchmark/turboPFOR benchmark/turboPFOR_bitpacking benchmarking/bitCompression benchmarking/bitCompressionSIMDCAI benchmarking/bitCompressionSIMDCAISeq benchmarking/bitCompressionSequential benchmarking/compressionUnencoded;
   do
-    git checkout "$branch_name" && git pull && git pull https://github.com/hyrise/hyrise $1 --no-edit && git push
+    git checkout "$branch_name" && git pull && git cherry-pick 0598a2040c4f4d5520b9d6710e795a47b28240d && git push
   done
