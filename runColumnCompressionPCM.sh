609bf6ca0a3a90e3ed515a6db5c6ccafc43ca727
root <you@example.com>
2021-03-15 09:05:00 +0000

    updates perf script

--- runColumnCompressionPCM.sh
+++ runColumnCompressionPCM.sh
@@ -8,16 +8,12 @@ run_benchmark() {
     if [ -n "$3" ]; then
        eval $3 && echo "setup"
     fi
-    mkdir -p $out_dir 
     mkdir -p cmake-build-release && cd cmake-build-release
     rm -rf *
     
     if cmake .. -DCMAKE_C_COMPILER=clang-$clang_version -DCMAKE_CXX_COMPILER=clang++-$clang_version -DCMAKE_BUILD_TYPE=Release -DHYRISE_RELAXED_BUILD=On -GNinja && ninja "$benchmark_name" ; then
-        perf stat -e "$to_measure" -o ../$out_dir/"$2"_baseline_sf"$scale_factor"_multi.txt ./"$benchmark_name" -e ../encoding_$2.json --dont_cache_binary_tables -r 1 -s $scale_factor --scheduler --clients $max_clients --mode=Shuffled >> ../$out_dir/sizes_$2_multi.txt 
-        perf stat -e "$to_measure" -o ../$out_dir/"$2"_baseline_sf"$scale_factor"_single.txt ./"$benchmark_name" -e ../encoding_$2.json --dont_cache_binary_tables -r 1 -s $scale_factor >> ../$out_dir/sizes_$2.txt
 
-        perf stat -e "$to_measure" -o ../$out_dir/"$2"_cache_sf"$scale_factor"_single.txt ./"$benchmark_name" -e ../encoding_$2.json --dont_cache_binary_tables -o ../$out_dir/tpch_$2_28_shuffled.json -t $max_time -s $scale_factor --scheduler --clients $max_clients --mode=Shuffled -r 500 >> ../$out_dir/sizes_$2_cache_multi.txt
-        perf stat -e "$to_measure" -o ../$out_dir/"$2"_cache_sf"$scale_factor"_multi.txt ./"$benchmark_name" -e ../encoding_$2.json --dont_cache_binary_tables -o ../$out_dir/tpch_$2_singlethreaded.json -r 500 >> ../$out_dir/sizes_$2_cache_single.txt
+        ./"$benchmark_name" -e ../encoding_$2.json --dont_cache_binary_tables -t $max_time -s $scale_factor --scheduler --clients $max_clients --mode=Shuffled
     fi
     cd ..
 }
@@ -29,7 +25,6 @@ scale_factor=3
 max_time=900
 benchmark_name="hyriseBenchmarkTPCH"
 to_measure=l2_rqsts.all_demand_data_rd,l2_rqsts.demand_data_rd_hit,l2_rqsts.demand_data_rd_miss,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses,LLC-prefetch-misses
-out_dir=$2
 
 
 if [ "$1" == "-tpch" ]; then
@@ -55,10 +50,6 @@ fi
 
 # Execute Benchmarks
 run_benchmark benchmarking/bitCompressionSIMDCAI bitpacking_simdcai "cd third_party/SIMDCompressionAndIntersection && make all -j 16 && cd -"
-run_benchmark benchmarking/bitCompression dictionary "cd third_party/TurboPFor-Integer-Compression && make all -j 8 && cd -"
-run_benchmark benchmarking/bitCompression bitpacking_turbopfor
-run_benchmark benchmark/compactVetor bitpacking_compactvector
-run_benchmark benchmark/compactVectorFixed bitpacking_compactvector_f
 run_benchmark benchmarking/compressionUnencoded compressionUnencoded
 
 # Process Result
