197192f6d195e97fd3e8799d4ac0d21a154c0300
Ben Hurdelhey <26392770+ben243871694@users.noreply.github.com>
2021-03-16 21:38:36 +0100

    update pcm script

--- runColumnCompressionPCM.sh
+++ runColumnCompressionPCM.sh
@@ -9,7 +9,6 @@ run_benchmark() {
        eval $3 && echo "setup"
     fi
     mkdir -p cmake-build-release && cd cmake-build-release
-    rm -rf *
     
     if cmake .. -DCMAKE_C_COMPILER=clang-$clang_version -DCMAKE_CXX_COMPILER=clang++-$clang_version -DCMAKE_BUILD_TYPE=Release -DHYRISE_RELAXED_BUILD=On -GNinja && ninja "$benchmark_name" ; then
 
@@ -22,7 +21,7 @@ run_benchmark() {
 clang_version=11
 max_clients=16
 scale_factor=3
-max_time=900
+max_time=500
 benchmark_name="hyriseBenchmarkTPCH"
 to_measure=l2_rqsts.all_demand_data_rd,l2_rqsts.demand_data_rd_hit,l2_rqsts.demand_data_rd_miss,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses,LLC-prefetch-misses
 
@@ -40,6 +39,8 @@ fi
 
 # Clone Hyrise Repo
 if [ ! -d hyriseColumnCompressionBenchmark ]; then
+   ssh-keygen
+   sudo apt update && sudo apt install git ninja-build build-essential clang zip cmake
     git clone git@github.com:benrobby/hyrise.git hyriseColumnCompressionBenchmark
     cd hyriseColumnCompressionBenchmark
     ./install_dependencies.sh
