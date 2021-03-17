8d953a5a7f4f4f6f7a7f9b1348ba69c57dfbae10
Linus Heinzl <linus.heinzl@student.hpi.de>
2021-03-17 20:52:17 +0100

    change zip name

--- runSegmentEncodingPerf.sh
+++ runSegmentEncodingPerf.sh
@@ -65,5 +65,5 @@ run_benchmark benchmark/turboPFOR LZ4
 run_benchmark benchmark/turboPFOR RunLength
 
 # Process Result
-zip -m ../columncompressionPerf$(date +%Y%m%d) tpch* sizes* "$out_dir/*"
+zip -m ../segmentEncoding$(date +%Y%m%d) tpch* sizes* "$out_dir/*"
 cd ..
