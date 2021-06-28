d9e86483c2dbc85e4bee5f07e8b252448e8be993
Martin.Boissier <martin.boissier@hpi.de>
2021-06-28 23:22:29 +0200

    M1 changes

--- runSegmentEncodingBenchmarkM1.sh
+++ runSegmentEncodingBenchmarkM1.sh
@@ -18,20 +18,20 @@ run_benchmark() {
           IFS=',' read item1 item2 <<< "${i}"
           cores=$item1
           max_clients=$item2
-          if ./cmake-build-release/"$benchmark_name" -e ./encoding_$2.json --dont_cache_binary_tables -o ./$benchmark_name_$2_"$max_clients"clients_${cores}cores_shuffled.json -t $max_time -s "$scale_factor" --cores $cores --scheduler --clients $max_clients --mode=Shuffled; then
+          if ./cmake-build-release/$benchmark_name -e ./encoding_${2}.json --dont_cache_binary_tables -o ./${benchmark_name}_${2}_${max_clients}lients_${cores}cores_shuffled.json -t $max_time -s $scale_factor --cores $cores --scheduler --clients $max_clients --mode=Shuffled; then
             echo "Success"
           else
             # scale factor not supported
-            ./cmake-build-release/"$benchmark_name" -e ./encoding_$2.json --dont_cache_binary_tables -o ./$benchmark_name_$2_"$max_clients"clients_${cores}cores_shuffled.json -t $max_time --cores $cores --scheduler --clients $max_clients --mode=Shuffled
+            ./cmake-build-release/"$benchmark_name" -e ./encoding_${2}.json --dont_cache_binary_tables -o ./${benchmark_name}_${2}_"$max_clients"clients_${cores}cores_shuffled.json -t $max_time --cores $cores --scheduler --clients $max_clients --mode=Shuffled
           fi
         done
       else
           cd ..
-          if ./cmake-build-release/"$benchmark_name" -e ./encoding_$2.json --dont_cache_binary_tables -o ./$benchmark_name_$2_singlethreaded.json -s ${scale_factor} >> ./sizes_$benchmark_name_$2.txt; then
+          if ./cmake-build-release/"$benchmark_name" -e ./encoding_${2}.json --dont_cache_binary_tables -o ./${benchmark_name}_${2}_singlethreaded.json -s ${scale_factor} >> ./sizes_${benchmark_name}_${2}.txt; then
             echo "Success"
           else
             # scale factor not supported
-            ./cmake-build-release/"$benchmark_name" -e ./encoding_$2.json --dont_cache_binary_tables -o ./$benchmark_name_$2_singlethreaded.json >> ./sizes_$benchmark_name_$2.txt   
+            ./cmake-build-release/"$benchmark_name" -e ./encoding_${2}.json --dont_cache_binary_tables -o ./${benchmark_name}_${2}_singlethreaded.json >> ./sizes_${benchmark_name}_${2}.txt
           fi
         fi
     else
@@ -41,8 +41,9 @@ run_benchmark() {
 
 # Configuration
 run_multithreaded=true
+max_time=1800
 benchmark_name="hyriseBenchmarkTPCH"
-scale_factor=3
+scale_factor=3  # use 10 for single-threaded comparisons; 3 for anything multi-threaded
 
 if [ "$1" == "-single" ]; then
   run_multithreaded=false
